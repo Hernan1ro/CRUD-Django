8a40a9c75cfe4f4f2c8d3b6608c8634e56428944
Hernan Mercado <hernandmf@gmail.com>
2022-12-10 13:22:00 -0500

    cusom user model

--- leads/models.py
+++ leads/models.py
@@ -1,19 +1,29 @@
 from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(AbstractUser):
+  pass
 
 # Create your models here.
 
 class Lead (models.Model):
+  
+  first_name = models.CharField(max_length=20)
+  last_name = models.CharField(max_length=20)
+  age = models.IntegerField(default=0)
+  agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+
   # SOURCE_CHOICES = (
   #   ("Youtube", "Youtube"),
   #   ("Google", "Google"),
   #   ("Newsletter", "Newsletter"),
-  # )
-
-  first_name = models.CharField(max_length=20)
-  last_name = models.CharField(max_length=20)
-  age = models.IntegerField(default=0)
+  # ) 
 
   # phoned = models.BooleanField(default=False)
   # source = models.CharField(default=SOURCE_CHOICES, max_length=100)
   # profile_picture = models.ImageField(blank=True, null=True)
   # special_files = models.FileField(blank=True, null=True)
+
+class Agent (models.Model):
+  user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
