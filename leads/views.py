e9ff0adddfd37cd222b0dcef03602aa2833cece3
Hernan Mercado <hernandmf@gmail.com>
2022-12-13 11:56:08 -0500

    templates in django

--- leads/views.py
+++ leads/views.py
@@ -3,4 +3,6 @@ from django.http import HttpResponse
 # Create your views here.
 
 def home_page(request):
-    return HttpResponse("Hello world")
+    # return HttpResponse("Hello world")
+    # return render(request, "leads/home_page.html")
+    return render(request, "second_page.html")
