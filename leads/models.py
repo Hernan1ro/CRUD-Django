b8a6d32921be8a0d215031fb77fc4edd32679672
Hernan Mercado <hernandmf@gmail.com>
2022-12-13 10:44:03 -0500

    querysets and model management

--- leads/models.py
+++ leads/models.py
@@ -12,6 +12,9 @@ class Lead (models.Model):
   last_name = models.CharField(max_length=20)
   age = models.IntegerField(default=0)
   agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+  def __str__(self):
+    return f"{self.first_name} {self.last_name}"
+
 
   # SOURCE_CHOICES = (
   #   ("Youtube", "Youtube"),
@@ -26,4 +29,6 @@ class Lead (models.Model):
 
 class Agent (models.Model):
   user = models.OneToOneField(User, on_delete=models.CASCADE)
+  def __str__(self):
+      return self.user.email
   
