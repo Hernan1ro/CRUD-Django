196929f2702e107331b20f2271f03115f4832982
Hernan Mercado <hernandmf@gmail.com>
2022-12-14 12:27:33 -0500

    context

--- leads/views.py
+++ leads/views.py
@@ -1,8 +1,13 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from .models import Lead
 # Create your views here.
 
 def home_page(request):
+    leads = Lead.objects.all()
+    context = {
+        "leads": leads,
+    }
     # return HttpResponse("Hello world")
     # return render(request, "leads/home_page.html")
-    return render(request, "second_page.html")
+    return render(request, "second_page.html", context)
