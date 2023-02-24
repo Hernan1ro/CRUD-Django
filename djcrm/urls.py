5d22f7963b84d4ea3fce7b059be4f9bdd4d638f8
Hernan Mercado <hernandmf@gmail.com>
2023-02-24 15:02:48 -0500

    [add] static files in django

--- djcrm/urls.py
+++ djcrm/urls.py
@@ -1,4 +1,6 @@
 
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from leads.views import landing_page, landingPageView
@@ -6,5 +8,10 @@ from leads.views import landing_page, landingPageView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', landingPageView.as_view(), name="landing-page"),
-    path('leads/', include('leads.urls', namespace='leads') )
+    path('leads/', include('leads.urls', namespace='leads') ),
 ]
+
+# for local development
+
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
